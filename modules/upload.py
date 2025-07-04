66c4ce376685cca282827472031e2a40b189c24c
gxd_pipeline <bc.lee@kr-geninus.com>
2025-07-04 15:18:31 +0900

    Minor Correction.

--- modules/upload.py
+++ modules/upload.py
@@ -235,7 +235,7 @@ def run_upload(args):
             print('FOLDER=' + ','.join( set([os.path.basename(a) for a in uniq_info[uniq_info['PRJ_TYPE']==item1['PRJ_TYPE']]['seqDir']]) ), file=f)
             print('tr_dir=' + tr_dir, file=f)
             print('JSONFILE=' + srcdir + '/info/' + now_str + '.' + item1['PRJ_TYPE'] + '.json', file=f)
-            print('ERRORFILE=' + srcdir + now_str + '.' + item1['PRJ_TYPE'] + '.error', file=f)
+            print('ERRORFILE=' + srcdir + '/' + now_str + '.' + item1['PRJ_TYPE'] + '.error', file=f)
             print("data_upload\nget_info\ncheck_size\n", file=f)
 
     if len(luck_samples) > 0 :
