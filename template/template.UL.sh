a3a428dc929e08ad2a8dacfaa1bb04372ed240da
gxd_pipeline <bc.lee@kr-geninus.com>
2025-07-04 15:21:35 +0900

    Minor Correction.

--- template/template.UL.sh
+++ template/template.UL.sh
@@ -23,15 +23,16 @@ function check_size(){
     for sub in `echo $FOLDER | sed -e "s/,/ /g"`; do
         cd $TMPDIR/$timestamp/$tr_dir/$sub
         for FILE in `ls ./*/*`; do
+            
             raw_size=$(stat -Lc %s "$FILE")
-            s3_info=$(aws s3api head-object --bucket "$tr_dir" --key "$sub/$FILE" 2>/dev/null)
+            s3_info=$(aws s3api head-object --bucket "$tr_dir" --key "$sub/${FILE/\.\//}" 2>/dev/null)
             if [ $? -ne 0 ]; then
-                echo "[ERROR] $timestamp/$tr_dir/$sub/$FILE not found in S3." >> $ERRORFILE
+                echo "[ERROR] $timestamp/$tr_dir/$sub/${FILE/\.\//} not found in S3." >> $ERRORFILE
                 continue
             fi
             s3_size=$(echo "$s3_info" | grep '"ContentLength"' | sed -E 's/[^0-9]*([0-9]+).*/\1/')
             if [ "$raw_size" -ne "$s3_size" ]; then
-                echo "[MISMATCH] $timestamp/$tr_dir/$sub/$FILE size mismatch." >> $ERRORFILE
+                echo "[MISMATCH] $timestamp/$tr_dir/$sub/${FILE/\.\//} size mismatch." >> $ERRORFILE
             fi
         done
     done
